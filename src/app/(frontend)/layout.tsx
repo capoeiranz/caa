aa033793fc29b72a64a6ab0487cc9d05516a44d5
4leite <4leite@gmail.com>
2026-02-22 22:34:51 +1300

    initial landing page

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,19 +1,25 @@
-import React from 'react'
 import './styles.css'
 
-export const metadata = {
-  description: 'A blank template using Payload in a Next.js app.',
-  title: 'Payload Blank Template',
+import type { ReactNode } from 'react'
+import type { Metadata } from 'next'
+
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Capoeira Angola Aotearoa',
+  description: 'Capoeira Angola Classes with Mestre Brabo',
 }
 
-export default async function RootLayout(props: { children: React.ReactNode }) {
-  const { children } = props
-
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: ReactNode
+}>) {
   return (
     <html lang="en">
-      <body>
-        <main>{children}</main>
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   )
 }
