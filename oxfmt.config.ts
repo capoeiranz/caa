e8a3d57c489bb893f971bcb274d389ccbd3c2fbb
4leite <4leite@gmail.com>
2026-05-17 01:20:39 +1200

    move to sst for deployments

--- oxfmt.config.ts
+++ oxfmt.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   semi: false,
   trailingComma: "all",
   proseWrap: "always",
-  ignorePatterns: ["src/routeTree.gen.ts", ".output", ".tanstack", ".sst"],
+  ignorePatterns: ["src/routeTree.gen.ts", ".output", ".tanstack", ".sst", "*.d.ts"],
   sortTailwindcss: {
     functions: ["clsx", "cx", "cva", "cn"],
   },
