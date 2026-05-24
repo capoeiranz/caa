4332c064f876803b21b012eaa447066ae63685d7
4leite <4leite@gmail.com>
2026-05-24 23:35:35 +1200

    fix browser side content negotiation

--- src/components/image.tsx
+++ src/components/image.tsx
@@ -1,7 +1,7 @@
 import { Image as BaseImage, Source, type ImageProps as UnpicImageProps } from "@unpic/react/base"
 
-const SUPPORTED_FORMATS = ["webp", "jpg","png"] as const
-type SupportedFormat = typeof SUPPORTED_FORMATS[number]
+const SUPPORTED_FORMATS = ["webp", "jpg", "png"] as const
+type SupportedFormat = (typeof SUPPORTED_FORMATS)[number]
 const TRANSFORMER_BASE = "https://unpic.invalid"
 
 type MediaOperations = {
@@ -14,8 +14,8 @@ type DistributiveOmit<T, K extends PropertyKey> = T extends unknown ? Omit<T, K>
 type ImageProps = DistributiveOmit<BaseImageProps, "transformer">
 
 function getExtension(filename: string): string {
-  const i = filename.lastIndexOf('.');
-  return i < 0 ? '' : filename.slice(i + 1);
+  const i = filename.lastIndexOf(".")
+  return i < 0 ? "" : filename.slice(i + 1)
 }
 
 function toSafeWidth(width: MediaOperations["width"]) {
@@ -27,18 +27,20 @@ function toSafeWidth(width: MediaOperations["width"]) {
   return Math.round(numericWidth)
 }
 
-const transform = (src: string | URL, operations: {
-  width?: number | string
-  format?: SupportedFormat | (string & {})
-} = {}) => {
-
+const transform = (
+  src: string | URL,
+  operations: {
+    width?: number | string
+    format?: SupportedFormat | (string & {})
+  } = {},
+) => {
   const sourceURL = new URL(src, TRANSFORMER_BASE)
   if (sourceURL.origin !== TRANSFORMER_BASE) {
     return String(src)
   }
 
   const pathname = sourceURL.pathname
-  
+
   const extension = getExtension(pathname)
   if (!SUPPORTED_FORMATS.includes(extension)) {
     return String(src)
@@ -47,7 +49,7 @@ const transform = (src: string | URL, operations: {
   const transformedPath = `/media${pathname}`
 
   const searchParams = new URLSearchParams()
- 
+
   const width = toSafeWidth(operations.width)
   if (width) {
     searchParams.set("w", String(width))
@@ -61,12 +63,10 @@ const transform = (src: string | URL, operations: {
 }
 
 export function Image(props: ImageProps) {
-  return <picture>
-  <Source
-    transformer={transform}
-    type="image/webp"
-    {...props}
-  />
-  <BaseImage transformer={transform} {...props} />
-</picture>
+  return (
+    <picture>
+      <Source transformer={transform} type="image/webp" {...props} />
+      <BaseImage transformer={transform} {...props} />
+    </picture>
+  )
 }
